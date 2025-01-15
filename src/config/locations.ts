ca399c523560af99238f6438bf3d8fa15dde8a07
Chan Meng <Chan.Meng@lincolnuni.ac.nz>
2025-01-15 22:25:28 +1300

    Update locations.ts

--- src/config/locations.ts
+++ src/config/locations.ts
@@ -4,19 +4,6 @@ import type { Location } from '@/types/location';
 export const locations: Location[] = [
   {
     id: '1',
-    name: '新华书店',
-    address: 'Q786+6VH, Ronggui Blvd Middle, Shunde District, Foshan, Guangdong Province, China, 528303',
-    position: {
-      lat: 22.766391234706237, 
-      lng: 113.26216873291828
-    },
-    type: 'mall',
-    status: 'active',
-    lastRefill: '2024-12-12',
-    availableUnits: 99
-  },
-  {
-    id: '2',
     name: '香港科技大学',
     address: 'Nansha District, Guangzhou, Guangdong Province, China, 511453',
     position: {
@@ -29,7 +16,7 @@ export const locations: Location[] = [
     availableUnits: 99
   },
   {
-    id: '3',
+    id: '2',
     name: '侨邦·国际公寓',
     address: 'No. 13, Rongqi Avenue, Shunde District, Foshan City, Guangdong Province, China, 528300',
     position: {
